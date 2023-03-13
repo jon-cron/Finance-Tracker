cb210fa091e3e8d9b9fd02bc2f683daf75701f53
jon-cron <jonathanacm95@gmail.com>
2023-03-13 17:17:31 -0600

    conditionally rendering auth buttons

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,24 +2,32 @@ import styles from "./Navbar.module.css";
 import { useLogout } from "../../hooks/useLogout.js";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useAuthContext } from "../../hooks/useAuthContext.js";
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>Finance</li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button onClick={logout} className="btn">
-            Logout
-          </button>
-        </li>
+        {!user && (
+          // NOTE since only one item can be return I surrounded the 2 list items with a fragment
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            <button onClick={logout} className="btn">
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
