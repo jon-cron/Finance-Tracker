eca7440e9a6c7f297a8f6154985812a1fae50e30
jon-cron <jonathanacm95@gmail.com>
2023-03-13 13:45:19 -0600

    firebase auth hook complete

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -4,5 +4,29 @@ export default useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   // NOTE email password and displayName are firebase specific for user info
-  const signUp = async (email, password, displayName) => {};
+  const signUp = async (email, password, displayName) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res.user);
+      if (!res) {
+        throw new Error("Could not signUp");
+      }
+      await res.user.updateProfile({ displayName });
+      setIsPending(false);
+      setError(null);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+      setIsPending(false);
+    }
+  };
+  {
+    error, isPending, signUp;
+  }
 };
